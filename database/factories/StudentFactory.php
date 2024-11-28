b63b937c8e5b8546cb5ee590e9014d24a9b5d351
Daniel Neto <danielneto314@gmail.com>
2024-11-28 12:43:58 -0300

    feat: implementa modelo 'Student' e o factory 'StudentFactory'

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Student>
+ */
+class StudentFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+        ];
+    }
+}
